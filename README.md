2bfb348da041965613199c85276ad9c1d85a7ea8
Spoderdan <dan.anghel55@gmail.com>
2023-05-28 19:17:08 +0300

    Add demo for observables and subjects

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # angular-observables
 
+Because observables produce values asynchronously, try/catch will not effectively catch errors. Instead, you handle errors by specifying an error callback on the observer. Producing an error also causes the observable to clean up subscriptions and stop producing values. An observable can either produce values (calling the next callback), or it can complete, calling either the complete or error callback.
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-nireae)
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { filter, from, map, Observable, of, retry, Subject } from 'rxjs';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   template: `
@@ -14,8 +16,54 @@ import { bootstrapApplication } from '@angular/platform-browser';
     </a>
   `,
 })
-export class App {
+export class App implements OnInit {
   name = 'Angular';
+
+  ngOnInit(): void {
+    this.observableDemo();
+  }
+
+  observableDemo() {
+    const observable = new Observable((subscriber) => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.error('My error');
+      subscriber.next(3);
+      subscriber.add(() => {
+        console.log('add');
+      });
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    console.log('just before subscribe');
+    observable.subscribe({
+      next(x) {
+        console.log('got value ' + x);
+      },
+      error(err) {
+        console.error('something wrong occurred: ' + err);
+      },
+      complete() {
+        console.log('done');
+      },
+    });
+    console.log('just after subscribe');
+  }
+
+  subjectDemo() {
+    const subject = new Subject();
+    subject.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('complete'),
+    });
+    subject.next('sub');
+    subject.complete();
+    subject.next('sub');
+    subject.next('sub');
+  }
 }
 
 bootstrapApplication(App);
